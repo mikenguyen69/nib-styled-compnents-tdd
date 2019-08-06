dc88385d8871c855ee8e04bbe5e20095ae14bb5e
Mike Nguyen <Mike.Nguyen@nib.co.nz>
2019-08-07 11:17:28 +1200

    Update CarouselButton.test.js

--- src/tests/CarouselButton.test.js
+++ src/tests/CarouselButton.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {configure, shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import CarouselButton from '../CarouselButton';
-import { on } from 'cluster';
 
 configure({adapter: new Adapter()});
 
